b5e9e8a53552c22365f87927d4c2567cfa4921b8
ZenKho <heruhoangphuc0601@gmail.com>
2025-10-06 19:35:23 +0700

    fix: add Maven publishing configuration for JitPack
    - Add maven-publish plugin to build.gradle.kts
    - Configure Maven publication with proper POM metadata
    - Update jitpack.yml to use publishToMavenLocal command
    - This resolves the 'No build artifacts found' issue on JitPack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    `maven-publish`
     alias(libs.plugins.lavalink)
 }
 
@@ -35,6 +36,36 @@ repositories {
     maven { url = uri("https://jitpack.io") }
 }
 
-repositories {
-    maven { url = uri("https://jitpack.io") }
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            
+            pom {
+                name.set("Lava XM Plugin")
+                description.set("A Lavalink plugin for playing XM/MOD tracker files")
+                url.set("https://github.com/ZenKho-chill/lava-xm-plugin")
+                
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                
+                developers {
+                    developer {
+                        id.set("ZenKho-chill")
+                        name.set("ZenKho")
+                    }
+                }
+                
+                scm {
+                    connection.set("scm:git:git://github.com/ZenKho-chill/lava-xm-plugin.git")
+                    developerConnection.set("scm:git:ssh://github.com:ZenKho-chill/lava-xm-plugin.git")
+                    url.set("https://github.com/ZenKho-chill/lava-xm-plugin/tree/main")
+                }
+            }
+        }
+    }
 }
